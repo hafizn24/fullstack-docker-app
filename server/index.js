519fa471904dfb2b13bcc92facbc86d8a9d2a44d
hafiz <hafizn24@gmail.com>
2025-08-31 13:17:19 +0800

    comment condition check NODE_ENV

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.get('/{*path}', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'public')));
+//   app.get('/{*path}', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//   });
+// }
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
