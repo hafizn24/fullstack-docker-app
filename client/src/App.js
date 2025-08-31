d608afa20ea7d9920b7b54b3dd34326a0880a8b5
hafiz <hafizn24@gmail.com>
2025-08-31 11:23:57 +0800

    added data display in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Layout from './components/Layout';
+import Home from './components/main/Home';
+
+const font = "'Inter', sans-serif"
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#8F329D',
+    },
+    secondary: {
+      main: '#7e57c2',
+    },
+    background: {
+      default: '#141414',
+      paper: '#6c6c6cff',
+    },
+  },
+  typography: {
+    fontFamily: font,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 🚀
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
